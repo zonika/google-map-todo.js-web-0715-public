04e7deedb1544dc391a2b882af2e85d85663f510
zonika <zchodosh@conncoll.edu>
2015-09-11 18:20:49 -0400

    done

--- public/javascripts/fetch_location.js
+++ public/javascripts/fetch_location.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
 
   function getLocation() {
     // code here
+    var loc = navigator.geolocation.getCurrentPosition(function(position){
+      $('#longitude').text(position.coords.longitude);
+      $('#latitude').text(position.coords.latitude);
+      insertMap(position.coords.latitude,position.coords.longitude)
+    });
   }
-
   getLocation();
 
 });
--- public/javascripts/insert_map.js
+++ public/javascripts/insert_map.js
@@ -1,3 +1,9 @@
 function insertMap(latitude, longitude) {
   // code here
+  latlon = new google.maps.LatLng(latitude, longitude);
+  var myOptions = {
+    center:latlon,zoom:14,
+    }
+
+  var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
 }
